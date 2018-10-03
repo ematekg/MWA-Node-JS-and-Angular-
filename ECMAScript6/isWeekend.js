515f6ba95f4053bcc8ad53d953c1f4dc0253a34c
ematekg <ematekg@yahoo.com>
2018-10-03 02:40:32 -0700

    initial commit

--- ECMAScript6/isWeekend.js
+++ ECMAScript6/isWeekend.js
@@ -1,5 +1,8 @@
-function isWeekEnd(){
-const todayDate=new Date();
-const day=todayDate.getDate();
+function isWeekend(){
 
+    const todayDate = new Date();
+    const day  = todayDate.getDay();
+    const days = ['weekend','weekday','weekday','weekday','weekday','weekday','weekend'];
+    return days[day];
 }
+console.log(isWeekend());
