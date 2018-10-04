d502408f24e256309f6b6c4252f1c4d1343e017d
ematekg <ematekg@yahoo.com>
2018-10-04 14:49:02 -0700

    update

--- NodeJs01/EvenOddArray.js
+++ NodeJs01/EvenOddArray.js
@@ -1,30 +1,31 @@
 
 function even(data){
 
-const d=data.reduce((b,e,i)=>{
-    if(e%2===0)
-    b.push(e);
-   return b;
-    },[]);
-    console.log(d);
-
-}
-
-function odd(data){
-    const d=data.reduce(function(b,e,i){
-       if(!(e%2===0))
-       b.push(e);
+    const d=data.reduce((b,e,i)=>{
+        if(e%2===0)
+        b.push(e);
        return b;
+        },[]);
+        console.log(d);
+    
+    }
+    
+    function odd(data){
+        const d=data.reduce(function(b,e,i){
+           if(!(e%2===0))
+           b.push(e);
+           return b;
+           
+           },[]);
        
-       },[]);
-   
-       console.log(d);
-   
-   }
-
-
-console.log('start');
-process.nextTick(even,[1,2,3,4,5,6,7,8]);
-process.nextTick(odd,[1,2,3,4,5,6,7,8]);
-console.log('end');
-
+           console.log(d);
+       
+       }
+    
+    
+    console.log('start');
+    process.nextTick(even,[1,2,3,4,5,6,7,8]);
+    process.nextTick(odd,[1,2,3,4,5,6,7,8]);
+    console.log('end');
+    
+    
